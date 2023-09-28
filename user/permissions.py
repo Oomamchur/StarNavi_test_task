919a2a92c212d339261564b420b097bd5a66ef0a
Oleksandr Mamchur <irex1988@gmail.com>
2023-09-28 17:57:23 +0300

    added fixture

--- user/permissions.py
+++ user/permissions.py
@@ -3,11 +3,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class ReadOnly(BasePermission):
     def has_permission(self, request, view) -> bool:
-        return bool(
-            request.method in SAFE_METHODS
-            and request.user
-            and request.user.is_authenticated
-        )
+        return request.method in SAFE_METHODS
 
 
 class IsCreatorOrReadOnly(BasePermission):
